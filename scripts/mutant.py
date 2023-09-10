c449482ddf4432b306f270bac51a4e1f0827c275
drime <spogocev@gmail.com>
2023-09-10 16:01:32 +0300

    lol

--- scripts/mutant.py
+++ scripts/mutant.py
@@ -14,7 +14,7 @@ def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as mutators:
         gr.Interface(fn=mutate_negative, inputs=["text", "text"], outputs="text",title="mutate negative")
         gr.Interface(fn=mutate_random_merge, inputs=["text","text","text"], outputs="text",title="mutate random merge")
-        gr.Interface(fn=mutate_add_diff, inputs=["text","text","text"], outputs="text",title="mutate add diff")
+        gr.Interface(fn=merge_by_name, inputs=["text","text","text","text"], outputs="text",title="merge by name")
 
         return [(mutators, "Mutator", "mutator_tab")]
 
@@ -77,29 +77,6 @@ def merge_by_name(input_path1,input_path2,output_path,names):
     save_file(tensors1, output_path)
 
     return f"Done, merged {merged} tensors."
-    
-
-def mutate_add_diff(input_path1,input_path2,output_path):
-    tensors1 = {}
-    tensors2 = {}
-    with safetensors.safe_open(input_path1, framework="pt", device="cpu") as f:
-        for key in f.keys():
-            tensors1[key] = f.get_tensor(key)
-    
-    with safetensors.safe_open(input_path2, framework="pt", device="cpu") as f:
-        for key in f.keys():
-            tensors2[key] = f.get_tensor(key)
-    
-    done = 0
-    for key in tensors1:
-        param1 = tensors1[key]
-        param2 = tensors2[key]
-        param = param1 - param2
-        tensors1[key] = param
-        done = done + 1
-    save_file(tensors1, output_path)
-
-    return f"Done, total {done} tensors."
            
 
   
